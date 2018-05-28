41f7585b79c3f1e88f7ce5008b67d063f1f5f5a6
TheUnholyGod <kitsonlow99@gmail.com>
2018-05-28 14:52:18 +0800

    FirstCommit

---
+++ RaiseEventTestPlugin/PackageDecoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlugin
+{
+    class PackageDecoder
+    {
+        public string GetValue(string src,string name)
+        {
+            if (!src.Contains(name))
+                return "Error";
+            string substr = src.Substring(src.IndexOf(name) + name.Length + 1);
+            string result = substr.Substring(0, substr.IndexOf(" "));
+            return result;
+        }
+    }
+}
---
+++ RaiseEventTestPlugin/RaiseEventTestPlugin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.Hive;
+using Photon.Hive.Plugin;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace TestPlugin
+{
+    public class RaiseEventTestPlugin : PluginBase
+    {
+        MySqlConnection conn;
+
+        public string ServerString
+        {
+            get;
+            private set;
+        }
+        public int CallsCount
+        {
+            get;
+            private set;
+        }
+        public RaiseEventTestPlugin()
+        {
+            this.UseStrictMode = true;
+            this.ServerString = "ServerMessage";
+            this.CallsCount = 0;
+
+            ConnectToSQL();
+        }
+        public override string Name
+        {
+            get
+            {
+                return this.GetType().Name;
+            }
+        }
+        public override void OnRaiseEvent(IRaiseEventCallInfo info)
+        {
+            try
+            {
+                base.OnRaiseEvent(info);
+            }
+            catch (Exception e)
+            {
+                this.PluginHost.BroadcastErrorInfoEvent(e.ToString(), info);
+                return;
+            }
+            if (1 == info.Request.EvCode)
+            {
+                string RecvdMessage = Encoding.Default.GetString((byte[])info.Request.Data);
+                PackageDecoder pc = new PackageDecoder();
+                string PlayerName = pc.GetValue(RecvdMessage, "PlayerName");
+                string Password = pc.GetValue(RecvdMessage, "Password");
+                
+            }
+        }
+
+        public void ConnectToSQL()
+        {
+            string connStr = "server=localhost;user=root;database=spaceships;port=3306;password=DM2341sidm;SslMode=none";
+            conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public void DisconnectFromSQL()
+        {
+            conn.Close();
+        }
+    }
+}
+
